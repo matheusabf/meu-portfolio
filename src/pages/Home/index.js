cb24de6441d1cac0c2ad224fd4d1e738246577e6
Matheus Ferreira <48792317+matheusabf@users.noreply.github.com>
2024-01-04 06:29:29 -0300

    Update Wpp button.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,7 +16,7 @@ function Home (){
                 </div>
                 <div className="col-3">
                     <span><a href="https://cdn.discordapp.com/attachments/1176727129245896905/1186628030127411231/Curriculo_Matheus.pdf?ex=6593f078&is=65817b78&hm=2cce851b6dce4d349118baa18106516dcbbb2f4f7f3ddd053a1f5a1e1a2bd641&">Baixar CV <i class="fas fa-arrow-down"></i></a></span><br/>
-                    <span className="btnconversa"><a href="https://api.whatsapp.com/send?phone=5511975507168&text=Ol%C3%A1%20Matheus,%20ap%C3%B3s%20olhar%20seu%20portf%C3%B3lio,%20gostaria%20de%20entrar%20em%20contato!" target="_blank" rel="noreferrer"><i class="fab fa-whatsapp"></i> VAMOS CONVERSAR</a></span>
+                    <span className="btnconversa"><a href="https://api.whatsapp.com/send?phone=5511975507168&text=Ol%C3%A1%20Matheus,%20gostaria%20de%20entrar%20em%20contato!" target="_blank" rel="noreferrer"><i class="fab fa-whatsapp"></i> VAMOS CONVERSAR</a></span>
                 </div>  
             </section>
         </div>
