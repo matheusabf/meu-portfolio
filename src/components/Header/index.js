b688dabbb4bfc7ee4e2e298f32da1325c88e39cb
Matheus Ferreira <48792317+matheusabf@users.noreply.github.com>
2024-01-08 20:02:43 -0300

    Update JS mobile.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 
 import { Link } from 'react-router-dom';
 import './header.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Links from '../../components/Links'
 
@@ -9,7 +9,6 @@ import Links from '../../components/Links'
 function Header() {
     
     const [isMenuOpen, setMenuOpen] = useState(false);
-
     const toggleMenu = () => {
         setMenuOpen(!isMenuOpen);
     };
@@ -18,9 +17,29 @@ function Header() {
             setMenuOpen(false);
         
     }
+    const [isVisible, setIsVisible] = useState(true);
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const isScrolledToTop = currentScrollTop === 0;
+  
+        if (isScrolledToTop || currentScrollTop < 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [setIsVisible]);
+    
     return (
-        <div className="headerDiv">
-            <header>
+        <div id="menu" className='headerDiv'>
+            <header id="menu" className={isVisible ? '' : 'hidden'}>
                 <Link className="logo" to="/">Home</Link>
                 <Link className="logo" to="/sobre">Sobre</Link>
                 <Link className="logo" to="/projetos">Projetos</Link>
@@ -28,11 +47,11 @@ function Header() {
                 <Link className="logo" to="/contato">Contato</Link>
             </header>
             <div className="menuHamb">
-            <div className="menu-btn" onClick={toggleMenu}>
+              <div className="menu-btn" onClick={toggleMenu}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
-            </div>
+              </div>
             </div>
             <nav className={`nav-menu ${isMenuOpen ? 'show-menu' : ''}`}>
                 <ul>
