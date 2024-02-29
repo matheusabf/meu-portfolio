30f0986bea50d99bcf7fbddb6f2390ac9d08cfe6
Matheus Ferreira <48792317+matheusabf@users.noreply.github.com>
2024-02-29 12:36:19 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,7 @@ function Home (){
                     <img className="perfil" src="https://github.com/matheusabf/meu-portfolio/assets/48792317/9f30d108-3bd2-45fe-a345-30172b5a5fdd" alt="imagem de perfil"></img>
                 </div>
                 <div className="col-3">
-                    <span><a href="https://cdn.discordapp.com/attachments/1176727129245896905/1186628030127411231/Curriculo_Matheus.pdf?ex=6593f078&is=65817b78&hm=2cce851b6dce4d349118baa18106516dcbbb2f4f7f3ddd053a1f5a1e1a2bd641&">Baixar CV <i class="fas fa-arrow-down"></i></a></span><br/>
+                    <span><a href="https://github.com/matheusabf/meu-portfolio/files/14450162/Curriculo_Matheus.pdf">Baixar CV <i class="fas fa-arrow-down"></i></a></span><br/>
                     <span className="btnconversa"><a href="https://api.whatsapp.com/send?phone=5511975507168&text=Ol%C3%A1%20Matheus,%20gostaria%20de%20entrar%20em%20contato!" target="_blank" rel="noreferrer"><i class="fab fa-whatsapp"></i> VAMOS CONVERSAR</a></span>
                 </div>  
             </section>
